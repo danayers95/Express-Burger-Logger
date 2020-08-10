2cf0aa6a5f9880ba26c116b7ebf0cf1a87cf792d
Donkey <danglayers@gmail.com>
2020-08-10 01:49:42 -0400

    fixed issues with seeds.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
+
+INSERT INTO burgers (burger_name) VALUES ("Double Cheeseburger");
+
+INSERT INTO burgers (burger_name) VALUES ("Vegan Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Lettuce Burger");
+
+INSERT INTO burgers (burgers_name) VALUES ("Chili Burger");
+
+INSERT INTO burgers (burgers_name) VALUES ("BBQ Burger");
