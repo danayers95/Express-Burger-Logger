f290ce47295edb528499608869f2aace09837304
Donkey <danglayers@gmail.com>
2020-08-13 19:33:29 -0400

    added callback functions

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,23 @@
 const orm = require("../config/orm.js");
+
+const burger = {
+    selectAll: function(callback) {
+        orm.selectAll(function(res) {
+            callback(res);
+        });
+    },
+
+    insertOne: function(burger_name, callback) {
+        orm.insertOne(burger_name, function(res) {
+            callback(res);
+        });
+    },
+
+    updateOne: function(burger_id, callback) {
+        orm.updateOne(burger_id, function(res) {
+            callback(res);
+        });
+    }
+};
+
+module.exports = burger;
