4f6129298275b3f03be468252911167e7bc9bc21
Donkey <danglayers@gmail.com>
2020-08-14 03:34:41 -0400

    made some changes/added comments

--- server.js
+++ server.js
@@ -1,27 +1,22 @@
-// require express, express-handlebars and body-parser
+// Require express, express-handlebars and body-parser
 const express = require("express");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 
-//calls express
+// Calls express
 const app = express();
 
-
-
-// opens the server that the application will be working on the local host
-const PORT = process.env.PORT || 3000;
-
+// Parse application
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.engine("handlebars", exphbs({
-    defaultlayout: "main"
-}));
-
+// Set up handlebars
+app.engine("handlebars", exphbs({defaultlayout: "main"}));
 app.set("view engine", "handlebars");
 
-app.use(routes);
+const router = require("./controllers/burger_controller.js");
+app.use("/", router);
 
-app.listen(PORT, function () {
-    console.log("Listening on Port: " + PORT);
-});
+// Opens server on localhost 
+const port = process.env.PORT || 3000;
+app.listen(port);
