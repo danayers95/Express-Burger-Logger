e5d42a97ecf2ce46e6a26b677e3356834c4b08ca
Donkey <danglayers@gmail.com>
2020-08-15 15:15:20 -0400

    getting closer to a functional product

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,81 @@
 // imports connection.js into orm.js
-const connection = require("./config/connection.js");
+const connection = require("./connection.js");
 
-connection.connect(function(err) {
+function printQuestionMarks(num) {
+    let arr = [];
+    for (let i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
+function objToSql(ob) {
+    let arr = [];
+
+    for (let key in ob) {
+        let value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+}
+
+const orm = {
+    all: function (tableInput, cb) {
+        connection.query("SELECT * FROM " + tableInput + ";", (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    create: function (table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    update: function (tables, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+};
+
+module.exports = orm;
+
+
+
+
+/* connection.connect(function(err) {
     if (err) {
         console.error("error connecting: " + err.stack);
         return;
@@ -33,4 +107,4 @@ const orm = {
     }
 };
 
-module.exports = orm;
+module.exports = orm; */
