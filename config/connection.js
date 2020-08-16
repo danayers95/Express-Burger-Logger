705b30cf1ddb3201ba778a6d62de289d43a0cc58
Donkey <danglayers@gmail.com>
2020-08-15 20:56:24 -0400

    tried out a different connection method

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 const mysql = require("mysql");
 const dotenv = require("dotenv").config();
 
-const connection = mysql.createConnection({
-    host: process.env.HOST,
-    port: 3000,
-    user: process.env.USER,
-    password: process.env.DB_PASS, 
-    database: "burgers_db",
-});
+// trying something out instead:
+var connection;
+if (process.env.JAWSDB_URL) {
+    console.log(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: 3000,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+    });
+}
 
 connection.connect(function(err) {
     if (err) {
