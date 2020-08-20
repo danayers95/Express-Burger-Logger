705dc6b1b5d2b612c4425217c90c3bcb73227985
Donkey <danglayers@gmail.com>
2020-08-20 18:33:15 -0400

    changed variable names and restructured some things for consistency

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-// Require express, express-handlebars and body-parser
+// dependencies for server file
 const express = require("express");
-const router = require("./controllers/burgers_controller.js");
+const body_parser = require("body-parser");
 const handlebars = require("express-handlebars");
 
 // Calls express
@@ -8,21 +8,22 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+
 app.use(express.static("public"));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(body_parser.urlencoded({ extended: true }));
+app.use(body_parser.json());
 
-// Set up handlebars
-app.engine(
-    "handlebars", 
-    handlebars({ 
-        layoutsDir: __dirname + "/views/layouts",
-    })
-);
+/* app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); */
+
+// set handlebars requirements
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.use('/', router);
+const routes = require("./controllers/burgers_controller.js");
+
+app.use('/', routes);
 
 // Opens server on localhost 
 app.listen(process.env.PORT || PORT, () => {
